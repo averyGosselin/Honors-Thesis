5f0ed0560b0316c856b47fd0d1635e8715cecc41
Avery Gosselin <avery.gosselin@maine.edu>
2023-03-06 08:52:56 -0500

    made simple login form

--- screens/ContributeScreen.js
+++ screens/ContributeScreen.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 
 export default function ContributeScreen({navigation}) {
   return (
-    <View>
+    <View style={styles.page}>
       <SafeAreaView>
-        <Text>Contribute Screen</Text>
+        <View style={styles.content}>
+          <Text>Welcome</Text>
+        </View>
       </SafeAreaView>
     </View>
   )
 }
+
+//Theme constants
+const WHITE = '#FCFAFA'
+const PLATINUM = '#CED8DF'
+const BLACK = '#000000'
+const GREEN = '#809848'
+const BLUE = '#2274A5'
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: PLATINUM,
+    flex: 1,
+  },
+  content: {
+    alignItems: 'center',
+  }
+})
